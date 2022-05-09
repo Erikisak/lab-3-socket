a5c955e11080a3e43b69420c2b348f65afc182d9
Erikisak <erik.isak@icloud.com>
2022-05-09 12:32:20 +0200

    components created

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Chat from "./components/Chat";
+import Homepage from "./components/Homepage";
+import Layout from "./components/Layout";
+import Rooms from "./components/Rooms";
+
 
 
 function App() {
   return (
-    <div >
-      Hello world
-    </div>
+    <BrowserRouter>
+    <Layout/>
+    <Routes>
+    <Route path="/" element={<Homepage />} />
+    <Route path="/rooms" element={<Rooms />} />
+    <Route path="/chat" element={<Chat />} />     
+    </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/Homepage.tsx
@@ -0,0 +1,67 @@
+import { Paper, SxProps, Typography, TextField, Box, Button, Grid } from "@mui/material";
+import Link from '@mui/material/Link';
+
+
+
+
+
+function Homepage() {
+    return (
+       
+        <Grid>
+        <Paper variant="outlined" elevation={10} sx={paper}>
+            <Grid >
+           <Typography sx={headText}>
+               Enter your nickname
+           </Typography>
+           </Grid>
+           
+           <TextField sx={textfield} id="outlined-basic" label="Nickname" variant="outlined"  required/>
+
+            <Grid>
+            <Link href="/Rooms" variant="body2">
+            <Button type="submit" variant="contained" sx={button} >
+                Submit
+            </Button>
+            </Link>
+            </Grid>
+            
+
+           
+        </Paper>
+        </Grid>
+    );
+     
+    
+  }
+  
+  const paper: SxProps = {
+      background: '',
+      marginTop: '5rem', 
+      height: '30rem',
+      width: '20rem',
+      marginRight: 'auto',
+      marginLeft: 'auto'
+  }
+  const headText: SxProps = {
+      marginTop: '2rem',
+      textAlign: 'center',
+      fontSize: '1.5rem'
+  }
+  const textfield: SxProps = {
+      marginTop: '6rem',
+      display: 'flex',
+      justifyContent: 'center',
+      width: '12rem',
+      marginLeft: 'auto',
+      marginRight: 'auto'
+
+  }
+  const button: SxProps = {
+      marginTop: '5rem',
+      marginLeft: '7rem',
+      marginRight: 'auto'
+
+  }
+
+  export default Homepage;
